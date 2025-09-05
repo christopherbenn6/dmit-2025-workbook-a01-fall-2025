52043540f4fddafaf54386cebaf6824a18abe97d
christopherbenn6 <111609212+christopherbenn6@users.noreply.github.com>
2025-09-05 11:25:44 -0600

    i did a thing

--- module-1/03-php-basics-exercises/problem-three.php
+++ module-1/03-php-basics-exercises/problem-three.php
@@ -2,12 +2,21 @@
     $title = "Problem Two - Pythagorean Theorem";
     include 'includes/header.php';
 
-    $fullNumber = '281078787';
-    $addedNumber = 0;
-    for($i = strlen(($fullNumber)); $i >= 0; $i--){
-        $addedNumber += intval(substr($fullNumber, $i, 1));
+    //Using Math and stuff
+    // $fullNumber = '281078787';
+    // $addedNumber = 0;
+    // for($i = strlen(($fullNumber)); $i >= 0; $i--){
+    //     $addedNumber += intval(substr($fullNumber, $i, 1));
+    // }
+    // echo "<p>$addedNumber</p>";
+
+    //treating a string as an array
+    $string = '1234';
+    $result = 0;
+    for($i = strlen($string) - 1; $i >= 0; $i--) {
+        $result += intval($string[$i]);
     }
-    echo "<p>$addedNumber</p>";
+    echo "<p>Adding all the digits of $string gives $result</p>";
 
 ?>
 
